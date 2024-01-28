3fa788c21d5bca3621b28484db1c9ee9b01e5256
xclhove <2959169303@qq.com>
2024-01-28 18:46:59 +0800

    refactor(layout): 修改备案号为可配置
    1. 修改备案号为可配置

---
+++ src/layout/Icp.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {Config, getConfig} from "../utils/config.ts";
+
+const icp = ref('')
+
+onMounted(() => {
+    getConfig((config: Config) => {
+        icp.value = config.icp
+    })
+})
+</script>
+
+<template>
+  <div class="footer" v-if="icp">
+    <span class="icp">
+      <a target="_blank" href="https://beian.miit.gov.cn" v-text="icp"></a>
+    </span>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: 5px 0;
+  background: white;
+
+  .icp {
+    a {
+      font-size: 14px;
+      color: #333333;
+      text-decoration: none;
+    }
+  }
+}
+</style>

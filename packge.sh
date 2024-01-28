dc7828ec8a78978985c3e7eea2d72dcdf49022d6
xclhove <2959169303@qq.com>
2024-01-28 18:43:36 +0800

    feat(root): 更新vite，修改打包脚本
    1. 更新vite到5.0.5
    2. 修改打包脚本

--- packge.sh
+++ packge.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# 切换到打包目录
 cd dist || exit 1
-tar -zcf xnote-vue.tar.gz ./* || exit 1
+
+# 压缩打包文件
+tar -zcf xnote-vue.tar.gz ./* || exit 2
+
+# 删除多余文件，只保留压缩文件
+find . -type f ! -name '*.tar.gz' -delete || exit 3
+find . -type d ! -name '*.tar.gz' -delete || exit 4
